e5fc2d76d9f2d73ac513d0b32b6603d1f6c68079
Peter Lauri <peterlauri@gmail.com>
2019-03-14 08:37:38 +0100

    card: basic structure for standard usage

---
+++ packages/card/mdwc-card.js
@@ -0,0 +1,60 @@
+import {
+  css,
+  html,
+  LitElement,
+} from 'lit-element';
+import {
+  classMap
+} from 'lit-html/directives/class-map.js';
+import {
+  MDCRipple
+} from '@material/ripple/index.js';
+import {
+  materialCss
+} from './material.css.js';
+
+class Card extends LitElement {
+
+  static get properties() {
+    return {
+      outlined: {
+        type: Boolean,
+      }
+    }
+  }
+
+  static get styles() {
+    return [
+      materialCss,
+      css `
+        :host {
+          display: block;
+        }
+        .mdc-button {
+          width: 100%;
+        }
+      `,
+    ];
+  }
+
+  render() {
+    const classes = {
+      'mdc-card--raised': this.raised,
+      // 'mdc-button--unelevated': this.unelevated,
+      // 'mdc-button--outlined': this.outlined,
+      // 'mdc-button--dense': this.dense,
+    };
+    return html `
+      <div class="mdc-card ${classMap(classes)}">
+        <slot></slot>
+      </div>
+    `;
+  }
+
+}
+
+export {
+  Card,
+};
+
+window.customElements.define('mdwc-card', Card);
