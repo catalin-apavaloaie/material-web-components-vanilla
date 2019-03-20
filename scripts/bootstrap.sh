e19eaaeaa96bad7edbf5b60c1a5c3f0adc205bda
Peter Lauri <peterlauri@gmail.com>
2019-03-20 19:09:15 +0200

    fix(env): bootstrap created wrong directory

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -4,7 +4,7 @@ set -x
 
 cwd=`pwd`
 
-mkdir -p "cwd/node_modeuls/@dolphin-kiss"
+mkdir -p "$cwd/node_modules/@dolphin-kiss"
 for package in `find packages -d 1 -type d`; do
   scripts/bootstrap-package.sh "$package"
 done
